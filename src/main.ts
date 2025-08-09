ed1e2bb7a58f6ecbfa7e773c6761ac33cdd93f24
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2025-08-09 13:10:36 +0800

    refactor: :recycle: 重构：提取到独立函数，减少缩进

--- src/main.ts
+++ src/main.ts
@@ -29,157 +29,162 @@ export default class CanvasReferencePlugin extends Plugin {
 		this.registerEvents();
 
 		// 📌【2025-07-10 00:34:00】快速添加键盘功能
-		this.registerDomEvent(document, "keydown", (e: KeyboardEvent) => {
+		this.registerDomEvent(document, "keydown", async (e: KeyboardEvent) => {
 			// @ts-ignore
 			const canvas: Canvas = this.app.workspace.getActiveViewOfType(ItemView)?.canvas as (Canvas | undefined)
 			if (!canvas) return;
-
-			const {
-				key, code,
-				ctrlKey, metaKey, altKey, shiftKey,
-			} = e
-
-			// 空格+节点 开始编辑（连边作用无效）
-			if ([' ', 'Enter'].includes(key) && !shiftKey && !ctrlKey && !altKey && !metaKey) {
-				const firstElement = canvas.selection.values()?.next()?.value
-				if (!firstElement) return;
-				const isEditing = firstElement?.isEditing
-				if (!isEditing) {
-					if (isCanvasNode(firstElement))
-						firstElement.startEditing()
-					// else if (isCanvasEdge(firstElement))
-					// 	firstElement.setLabel()
-				}
-			}
-			// x 删除选区
-			if (key === 'x' && canvas.selection.size > 0 && !shiftKey && !ctrlKey && !altKey && !metaKey) {
-				canvas.deleteSelection()
-			}
-			// q/Esc 取消编辑与选中
-			if (['q', 'Escape'].includes(key) && !shiftKey && !ctrlKey && !altKey && !metaKey) {
-				// ✅【2025-07-10 01:39:41】在结束文本编辑后，可取消编辑
-				if (canvas.selection.size > 0) {
-					canvas.deselectAll()
-				}
-			}
-			// c 调整颜色（shift反向）
-			if (code === 'KeyC' && !ctrlKey && !altKey && !metaKey) {
-				const MAX_COLOR_LENGTH = 7
-				for (const element of canvas.selection.values()) {
-					// 正在编辑的元素不修改颜色
-					if (isCanvasNode(element) && element.isEditing) continue
-					// 其它情况
-					if (isCanvasEdge(element) || isCanvasNode(element)) {
-						const color = Number(element.color)
-						const step = shiftKey ? MAX_COLOR_LENGTH - 1 : 1
-						if (isFinite(color)) {
-							const newColor = (color + step) % MAX_COLOR_LENGTH
-							element.setColor(newColor.toString())
-						}
-					}
-				}
-			}
-			// 选中+WASD：在节点之间移动选择
-			while (['KeyW', 'KeyA', 'KeyS', 'KeyD'].includes(code) && !ctrlKey && !altKey && !metaKey) {
-				// 若无选中节点⇒退出
-				if (!selectedNodes(canvas).next().value) break
-
-				// 获取按键对应的方向角
-				const rightDirectionDeg: number = { KeyD: 0, KeyS: 90, KeyA: 180, KeyW: 270 }[code]!
-				const rightDirectionRad = rightDirectionDeg * Math.PI / 180
-				// 限制角度范围，避免选中到边缘 | 此即：即便再近，也不会选中反方向的节点
-				const restrictedAngleRangeRad = 45 * Math.PI / 180
-
-				const transportedSelectedNodes = new Set<CanvasNode>()
-				for (const firstNode of selectedNodes(canvas)) {
-					const { x, y, width, height } = firstNode
-					const baseX = x + width / 2
-					const baseY = y + height / 2
-
-					// 计算距离最近的节点
-					let mostFit: { node: CanvasNode, distance: number } | undefined = undefined
-					for (const targetNode of canvas.nodes.values()) {
-						if (targetNode === firstNode) continue
-						const { x, y, width, height } = targetNode
-						const targetX = x + width / 2
-						const targetY = y + height / 2
-
-						let diffAngleRad = Math.atan2(targetY - baseY, targetX - baseX)
-						if (diffAngleRad < 0) diffAngleRad += 2 * Math.PI // 规范范围到 0 ~ 2π
-						const absDiffAngleRestricted = Math.abs(diffAngleRad - rightDirectionRad)
-						if (absDiffAngleRestricted > restrictedAngleRangeRad) continue
-
-						const distance = Math.sqrt((targetX - baseX) ** 2 + (targetY - baseY) ** 2)
-
-						mostFit ??= { node: targetNode, distance }
-						if (mostFit.distance > distance) {
-							mostFit.node = targetNode
-							mostFit.distance = distance
-						}
-					}
-
-
-					// 若有选中节点，则移动选择
-					if (!mostFit?.node) continue
-					transportedSelectedNodes.add(mostFit.node)
-				}
-
-				// 选中节点
-				if (transportedSelectedNodes.size <= 0) break
-				if (!shiftKey) canvas.deselectAll() // shift可以扩增选择
-				for (const node of transportedSelectedNodes)
-					canvas.select(node)
-				// 跟随选中：将画布平移到所有选中的节点处
-				// 改成for循环，一个循环计算4个值
-				const selectedBBox: BoundedBox = {
-					minX: undefined,
-					minY: undefined,
-					maxX: undefined,
-					maxY: undefined,
-				} as unknown as BoundedBox
-				for (const node of transportedSelectedNodes) {
-					const { minX, minY, maxX, maxY } = node.bbox
-					selectedBBox.minX ??= minX
-					selectedBBox.minX = Math.min(selectedBBox.minX, minX)
-					selectedBBox.minY ??= minY
-					selectedBBox.minY = Math.min(selectedBBox.minY, minY)
-					selectedBBox.maxX ??= maxX
-					selectedBBox.maxX = Math.max(selectedBBox.maxX, maxX)
-					selectedBBox.maxY ??= maxY
-					selectedBBox.maxY = Math.max(selectedBBox.maxY, maxY)
-				}
-				console.log(canvas)
-				canvas.panIntoView(selectedBBox)
-
-				break
-			}
-			// F「Focus」：单按 聚焦到选中的元素
-			if (code === 'KeyF' && !shiftKey && !ctrlKey && !altKey && !metaKey)
-				// 有选择⇒聚焦到选择
-				if (canvas.selection.size > 0) canvas.zoomToSelection()
-				// 没选择⇒选中离屏幕中心最近的节点
-				else {
-					const { minX, minY, maxX, maxY } = canvas.getViewportBBox()
-					const centerX = (minX + maxX) / 2
-					const centerY = (minY + maxY) / 2
-					let closestNode: CanvasNode | undefined = undefined
-					let closestDistance = Infinity
-					for (const node of canvas.nodes.values()) {
-						const { minX, minY, maxX, maxY } = node.bbox
-						const x = minX + (maxX - minX) / 2
-						const y = minY + (maxY - minY) / 2
-						const distance = Math.sqrt((x - centerX) ** 2 + (y - centerY) ** 2)
-						if (distance < closestDistance) {
-							closestNode = node
-							closestDistance = distance
-						}
-					}
-					if (closestNode) canvas.select(closestNode)
-				}
-			// Z「Zoom」：单按 放大，Shift 缩小
-			if (code === 'KeyZ' && !ctrlKey && !altKey && !metaKey)
-				canvas.zoomBy(shiftKey ? -0.1 : 0.1)
+			await this.onCanvasKeyDown(e, canvas)
 		})
+	}
+
+	// 独立出的功能：白板中键盘按下的功能
+	async onCanvasKeyDown(e: KeyboardEvent, canvas: Canvas) {
+		const {
+			key, code,
+			ctrlKey, metaKey, altKey, shiftKey,
+		} = e
+
+		// 空格+节点 开始编辑（连边作用无效）
+		if ([' ', 'Enter'].includes(key) && !shiftKey && !ctrlKey && !altKey && !metaKey) {
+			const firstElement = canvas.selection.values()?.next()?.value
+			if (!firstElement) return;
+			const isEditing = firstElement?.isEditing
+			if (!isEditing) {
+				if (isCanvasNode(firstElement))
+					firstElement.startEditing()
+				// else if (isCanvasEdge(firstElement))
+				// 	firstElement.setLabel()
+			}
+		}
+		// x 删除选区
+		if (key === 'x' && canvas.selection.size > 0 && !shiftKey && !ctrlKey && !altKey && !metaKey) {
+			canvas.deleteSelection()
+		}
+		// q/Esc 取消编辑与选中
+		if (['q', 'Escape'].includes(key) && !shiftKey && !ctrlKey && !altKey && !metaKey) {
+			// ✅【2025-07-10 01:39:41】在结束文本编辑后，可取消编辑
+			if (canvas.selection.size > 0) {
+				canvas.deselectAll()
+			}
+		}
+		// c 调整颜色（shift反向）
+		if (code === 'KeyC' && !ctrlKey && !altKey && !metaKey) {
+			const MAX_COLOR_LENGTH = 7
+			for (const element of canvas.selection.values()) {
+				// 正在编辑的元素不修改颜色
+				if (isCanvasNode(element) && element.isEditing) continue
+				// 其它情况
+				if (isCanvasEdge(element) || isCanvasNode(element)) {
+					const color = Number(element.color)
+					const step = shiftKey ? MAX_COLOR_LENGTH - 1 : 1
+					if (isFinite(color)) {
+						const newColor = (color + step) % MAX_COLOR_LENGTH
+						element.setColor(newColor.toString())
+					}
+				}
+			}
+		}
+		// 选中+WASD：在节点之间移动选择
+		while (['KeyW', 'KeyA', 'KeyS', 'KeyD'].includes(code) && !ctrlKey && !altKey && !metaKey) {
+			// 若无选中节点⇒退出
+			if (!selectedNodes(canvas).next().value) break
+
+			// 获取按键对应的方向角
+			const rightDirectionDeg: number = { KeyD: 0, KeyS: 90, KeyA: 180, KeyW: 270 }[code]!
+			const rightDirectionRad = rightDirectionDeg * Math.PI / 180
+			// 限制角度范围，避免选中到边缘 | 此即：即便再近，也不会选中反方向的节点
+			const restrictedAngleRangeRad = 45 * Math.PI / 180
+
+			const transportedSelectedNodes = new Set<CanvasNode>()
+			for (const firstNode of selectedNodes(canvas)) {
+				const { x, y, width, height } = firstNode
+				const baseX = x + width / 2
+				const baseY = y + height / 2
+
+				// 计算距离最近的节点
+				let mostFit: { node: CanvasNode, distance: number } | undefined = undefined
+				for (const targetNode of canvas.nodes.values()) {
+					if (targetNode === firstNode) continue
+					const { x, y, width, height } = targetNode
+					const targetX = x + width / 2
+					const targetY = y + height / 2
+
+					let diffAngleRad = Math.atan2(targetY - baseY, targetX - baseX)
+					if (diffAngleRad < 0) diffAngleRad += 2 * Math.PI // 规范范围到 0 ~ 2π
+					const absDiffAngleRestricted = Math.abs(diffAngleRad - rightDirectionRad)
+					if (absDiffAngleRestricted > restrictedAngleRangeRad) continue
+
+					const distance = Math.sqrt((targetX - baseX) ** 2 + (targetY - baseY) ** 2)
+
+					mostFit ??= { node: targetNode, distance }
+					if (mostFit.distance > distance) {
+						mostFit.node = targetNode
+						mostFit.distance = distance
+					}
+				}
+
+
+				// 若有选中节点，则移动选择
+				if (!mostFit?.node) continue
+				transportedSelectedNodes.add(mostFit.node)
+			}
+
+			// 选中节点
+			if (transportedSelectedNodes.size <= 0) break
+			if (!shiftKey) canvas.deselectAll() // shift可以扩增选择
+			for (const node of transportedSelectedNodes)
+				canvas.select(node)
+			// 跟随选中：将画布平移到所有选中的节点处
+			// 改成for循环，一个循环计算4个值
+			const selectedBBox: BoundedBox = {
+				minX: undefined,
+				minY: undefined,
+				maxX: undefined,
+				maxY: undefined,
+			} as unknown as BoundedBox
+			for (const node of transportedSelectedNodes) {
+				const { minX, minY, maxX, maxY } = node.bbox
+				selectedBBox.minX ??= minX
+				selectedBBox.minX = Math.min(selectedBBox.minX, minX)
+				selectedBBox.minY ??= minY
+				selectedBBox.minY = Math.min(selectedBBox.minY, minY)
+				selectedBBox.maxX ??= maxX
+				selectedBBox.maxX = Math.max(selectedBBox.maxX, maxX)
+				selectedBBox.maxY ??= maxY
+				selectedBBox.maxY = Math.max(selectedBBox.maxY, maxY)
+			}
+			console.log(canvas)
+			canvas.panIntoView(selectedBBox)
+
+			break
+		}
+		// F「Focus」：单按 聚焦到选中的元素
+		if (code === 'KeyF' && !shiftKey && !ctrlKey && !altKey && !metaKey)
+			// 有选择⇒聚焦到选择
+			if (canvas.selection.size > 0) canvas.zoomToSelection()
+			// 没选择⇒选中离屏幕中心最近的节点
+			else {
+				const { minX, minY, maxX, maxY } = canvas.getViewportBBox()
+				const centerX = (minX + maxX) / 2
+				const centerY = (minY + maxY) / 2
+				let closestNode: CanvasNode | undefined = undefined
+				let closestDistance = Infinity
+				for (const node of canvas.nodes.values()) {
+					const { minX, minY, maxX, maxY } = node.bbox
+					const x = minX + (maxX - minX) / 2
+					const y = minY + (maxY - minY) / 2
+					const distance = Math.sqrt((x - centerX) ** 2 + (y - centerY) ** 2)
+					if (distance < closestDistance) {
+						closestNode = node
+						closestDistance = distance
+					}
+				}
+				if (closestNode) canvas.select(closestNode)
+			}
+		// Z「Zoom」：单按 放大，Shift 缩小
+		if (code === 'KeyZ' && !ctrlKey && !altKey && !metaKey)
+			canvas.zoomBy(shiftKey ? -0.1 : 0.1)
+
 	}
 
 
